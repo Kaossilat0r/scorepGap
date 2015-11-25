75b8a18e4796d72a70d64f2861c359b89d159dfd
roman <kaossilat0r.rn@gmail.com>
2015-11-25 13:07:10 +0100

    break is not necessary in script

--- runInEnv.sh
+++ runInEnv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-module unload gcc scorep papi && \ 
-module load scorep/gcc/ompi/1.4.2 gcc/4.9.3 papi && \
+module unload gcc scorep papi && 
+module load scorep/gcc/ompi/1.4.2 gcc/4.9.3 papi && 
 make 
